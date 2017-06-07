f5daa4dc4f47d9d38cb077d8caa4f2f019f3fc07
laileon <leon@igola.com>
2017-06-07 19:39:28 +0800

    spring sec part1.8

--- src/main/java/com/clem/www/config/WebSecurityConfig.java
+++ src/main/java/com/clem/www/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/js/**", "/css/**", "/images/**", "/**/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
