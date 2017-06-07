c56615c4c5c10ce4cca4c9e98e308edbaffaf8ed
laileon <leon@igola.com>
2017-06-07 23:40:28 +0800

    spring sec 2.0

---
+++ src/main/java/com/clem/www/config/InMemorySecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.clem.www.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+
+@Configuration
+@EnableGlobalAuthentication
+public class InMemorySecurityConfiguration {
+
+       @Autowired
+       public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+               auth.inMemoryAuthentication().withUser("user").password("root").roles("USER")
+                               .and().withUser("admin").password("root").roles("USER", "ADMIN");
+       }
+}
